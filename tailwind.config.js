df3e47338b8085a7262bd3f853cc8c2364018744
Liker <79975604+AstonishedLiker@users.noreply.github.com>
2024-10-23 13:40:17 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        rubik: ["Rubik", "sans-serif"],
+      },
+      fontSize: {
+        'label': '13px',
+        'small-title': '20px',
+        'card-title': '16px',
+        'card-description': '13px',
+        'card-link': '11px',
+      },
+      colors: {
+        'card-color': '#e9e9e9',
+        'icon-color': '#e3e5ea',
+        'button-link-color': '#f0f0f0',
+        'image-border-color': '#c9c9c9',
+        'button-image-hover-color': '#f7f7f7',
+      }
+    },
+  },
+  plugins: [
+    require("@designbycode/tailwindcss-text-stroke"),
+  ],
+}
